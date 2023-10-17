47d9862bf57da5dde47852f6b1f6408ae11e1b26
Maksew <m1x6me@gmail.com>
2023-10-17 23:57:49 +0200

    Correction balise @endforeach,         Affichage des véhicules

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -197,14 +197,13 @@
                     </div>
                 </div>
             </div>
-            @endforeach
-
-
         </div>
+        @endforeach
+</div>
 
-        <!-- Bootstrap and jQuery Scripts -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Bootstrap and jQuery Scripts -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
